b548704986fe9ae9e21e5da2098ed529ea1e5773
zinx <63958372+zinx28@users.noreply.github.com>
2024-12-12 21:24:36 +0000

    fixed building exe
    Removed other build options

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -10,11 +10,13 @@ export default defineConfig({
     plugins: [externalizeDepsPlugin()]
   },
   renderer: {
+    
     resolve: {
       alias: {
         '@renderer': resolve('src/renderer/src')
       }
     },
     plugins: [vue()]
-  }
+    
+  },
 })
--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -5,7 +5,7 @@ import LoginScreen from './components/LoginScreen.vue'
 
 const isLoading = ref(true)
 // WIP (seeing what i can do)
-const ipcHandle = () => window.electron.ipcRenderer.send('ping') //  <a target="_blank" rel="noreferrer" @click="ipcHandle">Send IPC</a>
+//const ipcHandle = () => window.electron.ipcRenderer.send('ping') //  <a target="_blank" rel="noreferrer" @click="ipcHandle">Send IPC</a>
 
 // REMOVE FOR PROD THIS IS TO SKIP STUFF
 onMounted(() => {
