85c24f4318d23d730a26f8a13cd98f1cb0b5a016
zinx <63958372+zinx28@users.noreply.github.com>
2024-12-18 21:12:07 +0000

    i would say sidebar is done for now?

--- src/renderer/src/components/Dashboard/Nav.vue
+++ src/renderer/src/components/Dashboard/Nav.vue
@@ -4,14 +4,17 @@
     <ul class="nav-links">
       <li>
         <a href="#" class="nav-active">
-          <svg viewBox="0 0 22 22" class="icon" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="icon"
+            fill="currentColor"
+            viewBox="0 0 16 16"
+          >
             <path
-              d="M18.6828 9.96875L17.1875 8.47447V4.8125C17.1875 4.24531 16.7234 3.78125 16.1562 3.78125H15.125C14.5578 3.78125 14.0938 4.24531 14.0938 4.8125V5.38278L12.0312 3.32234C11.7497 3.05628 11.4919 2.75 11 2.75C10.5081 2.75 10.2503 3.05628 9.96875 3.32234L3.31719 9.96875C2.99544 10.3039 2.75 10.5483 2.75 11C2.75 11.5806 3.1955 12.0312 3.78125 12.0312H4.8125V18.2188C4.8125 18.7859 5.27656 19.25 5.84375 19.25H7.90625C8.47579 19.25 8.9375 18.7883 8.9375 18.2188V14.0938C8.9375 13.5266 9.40156 13.0625 9.96875 13.0625H12.0312C12.5984 13.0625 13.0625 13.5266 13.0625 14.0938V18.2188C13.0625 18.7883 13.0086 19.25 13.5781 19.25H16.1562C16.7234 19.25 17.1875 18.7859 17.1875 18.2188V12.0312H18.2188C18.8045 12.0312 19.25 11.5806 19.25 11C19.25 10.5483 19.0046 10.3039 18.6828 9.96875Z"
-              stroke="white"
-              stroke-width="2"
-              stroke-linejoin="round"
+              d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293zM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5z"
             />
           </svg>
+
           Home
         </a>
       </li>
@@ -34,7 +37,23 @@
     </ul>
     <div class="bottom">
       <div class="info">
-        <div class="InfoBox"></div>
+        <div class="InfoBox">
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M12 12V7.5M12 15.3354V15.375M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z"
+              stroke="white"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+        </div>
         <a class="BuildTitle">Public Build Available!</a>
         <a class="BuildS">Season 2</a>
         <a class="BuildB">1.11-CL-3807424</a>
@@ -42,11 +61,27 @@
       </div>
       <div class="split"></div>
       <div class="UserDetails">
-        <div class="profilePicture" :style="profileStyle"></div>
+        <div class="profilePicture" :style="profileStyle">
+          <div class="profile-indicator"></div>
+        </div>
 
         <div class="profile-details">
           <span class="username">{{ user.username }}</span>
           <span class="role" :style="roleStyle">{{ user.RoleName }}</span>
+     
+        </div>
+
+        <div class="leftButton">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            class="bi bi-gear-fill"
+            viewBox="0 0 16 16"
+          >
+            <path
+              d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z"
+            />
+          </svg>
         </div>
       </div>
     </div>
