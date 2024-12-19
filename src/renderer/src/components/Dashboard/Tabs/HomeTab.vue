f0cb6e1c8db03653f02ca85fac58e48c55687cfe
zinx <63958372+zinx28@users.noreply.github.com>
2024-12-19 23:06:13 +0000

    quickly do a few things for home page!
    got lazy and used a grid but looks decent

--- src/renderer/src/components/Dashboard/Tabs/HomeTab.vue
+++ src/renderer/src/components/Dashboard/Tabs/HomeTab.vue
@@ -1,6 +1,32 @@
 <template>
-    i have these lucid dreams<br><br>penis
+  <div class="outer-class">
+    <div class="PlayerClass">
+      <div class="inner-div">
+        <div class="pl-icon" :style="profileStyle"><!--ICON--></div>
+        <div class="text-container">
+          <span class="greeting">Hi, player!</span>
+          <span class="description">Relive OG Fortnite <b>Season 2</b> with <b>Luna!</b></span>
+        </div>
+      </div>
+    </div>
+    <div class="UserData">
+      <div class="UserSection"></div>
+      <div class="UserSection"></div>
+    </div>
+  </div>
 </template>
 
+<script>
+import imagePath from '../../../assets/tempplayer.png'
+export default {
+  computed: {
+    profileStyle() {
+      return {
+        backgroundImage: `url(${imagePath})`
+      }
+    }
+  }
+}
+</script>
 
 <style src="../../../assets/HomePage.css"></style>
