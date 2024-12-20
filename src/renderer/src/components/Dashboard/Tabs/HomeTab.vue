8b47bcc9478ae5bd4c7404842666246513208485
zinx <63958372+zinx28@users.noreply.github.com>
2024-12-20 21:18:05 +0000

    BBC (??!!?!)

--- src/renderer/src/components/Dashboard/Tabs/HomeTab.vue
+++ src/renderer/src/components/Dashboard/Tabs/HomeTab.vue
@@ -10,19 +10,45 @@
       </div>
     </div>
     <div class="UserData">
-      <div class="UserSection"></div>
-      <div class="UserSection"></div>
+      <div class="UserSection">
+         <div class="BTClass">
+          <span class="UserBTValue">21</span>
+          <span class="UserBTDec">Battle Pass Tier</span>
+         </div>
+
+         <div class="UserImage" :style="BpTier" ><!--ICON--></div>
+      </div>
+      <div class="UserSection">
+         <div class="BTClass">
+          <span class="UserBTValue">21</span>
+          <span class="UserBTDec">Season Level</span>
+         </div>
+
+         <div class="UserImage" :style="BpLevel" ><!--ICON--></div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import imagePath from '../../../assets/tempplayer.png'
+import BPTierPath from '../../../assets/BPTier.png'
+import BPLevelPath from '../../../assets/BPLevel.png'
 export default {
   computed: {
     profileStyle() {
       return {
         backgroundImage: `url(${imagePath})`
+      }
+    },
+    BpTier() {
+      return {
+        backgroundImage: `url(${BPTierPath})`
+      }
+    },
+    BpLevel() {
+      return {
+        backgroundImage: `url(${BPLevelPath})`
       }
     }
   }
