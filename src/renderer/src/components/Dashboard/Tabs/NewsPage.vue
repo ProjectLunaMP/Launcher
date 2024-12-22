3dd7e188d2ac0148795f788ae0fb038028195226
zinx <63958372+zinx28@users.noreply.github.com>
2024-12-22 23:35:11 +0000

    remove safes-x

--- src/renderer/src/components/Dashboard/Tabs/NewsPage.vue
+++ src/renderer/src/components/Dashboard/Tabs/NewsPage.vue
@@ -7,11 +7,7 @@
 
       <div class="NewsContent">
         <!--News Stuff forced in this div!-->
-        <strong>The Importance of Safe Sex: Protecting Your Health and Future</strong>
-        <p>Safe sex is an essential part of maintaining your health and well-being. It involves taking precautions to reduce the risk of sexually transmitted infections (STIs) and unintended pregnancies. Practicing safe sex isn’t just about using protection; it’s about fostering open communication, making informed decisions, and showing respect for yourself and your partner.</p>
-        <br>
-        <strong>Why Safe Sex Matters</strong>
-        <p>STIs, such as HIV, chlamydia, and gonorrhea, can have serious health consequences if left untreated. Using condoms or dental dams significantly lowers the risk of transmission. Additionally, safe sex helps prevent unplanned pregnancies, giving you greater control over your future.</p>
+        <strong>News Failed To Load</strong>
     </div>
     </div>
   </div>
