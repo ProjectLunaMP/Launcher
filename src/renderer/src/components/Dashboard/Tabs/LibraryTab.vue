ea37f2c059468375f14b66488f6b21bbbec474da
zinx <63958372+zinx28@users.noreply.github.com>
2024-12-23 14:49:40 +0000

    clock and started lib

--- src/renderer/src/components/Dashboard/Tabs/LibraryTab.vue
+++ src/renderer/src/components/Dashboard/Tabs/LibraryTab.vue
@@ -1,7 +1,36 @@
 <template>
-    do you?
+  <div class="outer-class">
+    <div class="LIBItemsGrid">
+      <div class="LibItem">
+        <div class="LibItemOuter">
+          <div class="LibItemBottom">
+            <span class="GameNameF"> Fortnite </span>
+            <span class="GameVersion"> 1.11-CL-3807424 </span>
+          </div>
+        </div>
+      </div>
+      <div class="LibItem">
+        <div class="LibItemOuter">
+          <div class="LibItemBottom">
+            <span class="GameNameF"> Fortnite </span>
+            <span class="GameVersion"> 1.11-CL-3807424 </span>
+          </div>
+        </div>
+      </div>
+      <!--This one is forced and actually different-->
+      <div class="LibItemForced">
+        <div class="LibItemOuter">
+          <div class="LibItemBottomForced">
+            <span class="ImportFR">Import</span>
+            <span class="ImportDec">Add a new instance of Fortnite.</span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- do you?
 
-    <button @click="launchGame">Launch!!!!</button>
+   <button @click="launchGame">Launch!!!!</button> -->
+  </div>
 </template>
 
 <script>
@@ -9,13 +38,15 @@ export default {
   methods: {
     async launchGame() {
       try {
-        const gameExePath = 'G:\\fortnite builds\\27548a59-417a-4b57-aeb9-9fe615855c31';
-        const dllPath = 'C:\\Users\\Zephironyx\\AppData\\Roaming\\FortLauncher\\FortCurl.dll';
-        window.electron.ipcRenderer.send('luna:launchgame', { gameExePath, dllPath });
+        const gameExePath = 'G:\\fortnite builds\\27548a59-417a-4b57-aeb9-9fe615855c31'
+        const dllPath = 'C:\\Users\\Zephironyx\\AppData\\Roaming\\FortLauncher\\FortCurl.dll'
+        window.electron.ipcRenderer.send('luna:launchgame', { gameExePath, dllPath })
       } catch (error) {
-        alert('Error launching game');
+        alert('Error launching game')
       }
     }
   }
 }
 </script>
+
+<style src="../../../assets/LibraryPage.css"></style>
